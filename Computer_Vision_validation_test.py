c7ff473358f603991c5644cfb52f3fcfe0e760ff
Jeremy Banta <37882190+jeremybanta@users.noreply.github.com>
2018-04-25 19:58:31 -0400

    Add files via upload

--- Computer_Vision_validation_test.py
+++ Computer_Vision_validation_test.py
@@ -19,7 +19,9 @@ os.chdir("C:\\Users\\Jeremy\\Desktop\\Senior Project Design");
 start_time=time.time();
 plot.close("all")
 gc.collect();
-model=load_model("model_final.h5");
+model=load_model("model.h5");
+model2=load_model("model_final.h5")
+#model3=load_model("model_deep_learning.h5")
 gc.collect();
 data=pickle.load(open("training_data.pkl","rb"));
 validation_Images=data[1];
@@ -29,7 +31,12 @@ gc.collect();
 del data;
 
 prediction_values=model.predict(validation_Images[0:720]);
-theta_angle=getAngle(prediction_values);
+prediction_values2=model2.predict(validation_Images[0:720]);
+#prediction_values3=model3.predict(validation_Images[0:720]);
+theta_angle=getAngle(prediction_values)+getAngle(prediction_values2);
+theta_angle=theta_angle/2;
+theta_angle=np.mod(theta_angle,360)
+
 error=getError(theta_angle,ground_truth_values);
 plot.figure(1);
 plot.plot(np.array(ground_truth_values[0:360]),np.array(error[0:360]));
--- int_main.py
+++ int_main.py
@@ -12,19 +12,24 @@ the user wants to add the implementation of these algorithms to the
 Computer Vision System
 
 '''
-
+import os;
+os.chdir("C:/Users/Jeremy/Desktop/Senior Project Design")
 
 import numpy as np
+from operator import itemgetter
 import pickle
 import cv2
+import time;
+from segment_blue_disk import segment_blue_disk
 import os
-
-os.chdir("C:/Users/Jeremy/Disktop/Senior Project Design")
-
 from template_matching import get_blue_disk_angle;
 
 cap=cv2.VideoCapture(1);
 
+
+
+
+training_type=input('enter what type you want to test performance on? ')
 
 def Main():
     
@@ -36,8 +41,8 @@ def Main():
         
         if(ret):
             
+            frame=segment_blue_disk(frame)
             theta=get_blue_disk_angle(frame)
-            
             cv2.imshow('frame',frame);
             cv2.waitKey()
             
@@ -47,13 +52,52 @@ def Main():
                 
             print(theta);
             
+    cap.release();
+    cv2.destroyAllWindows();
+            
+if(training_type=='matching'):
     
-        
-        
-        
-        
-        
-        
-        
-        
+    Main();
     
+if(training_type=='cnn'):
+    
+    from keras.models import load_model
+    from processing_libs import getAngle,normalize
+
+    model=load_model("model.h5");
+    model2=load_model("model_final.h5")
+    model3=load_model("model_deep_learning.h5")
+
+    
+    while True:
+        
+        
+        ret,frame=cap.read();
+
+        
+        if(ret):
+            
+            start_time=time.time()
+            frame=segment_blue_disk(frame);
+            frame=cv2.resize(frame,(215,215))
+            cv2.imshow('frame',frame)
+            cv2.waitKey()
+            frame=cv2.medianBlur(frame,5)
+            frame=normalize(frame);
+            cos_sin_vector=model.predict(np.reshape(frame,(1,215,215,3)));
+            cos_sin_vector2=model2.predict(np.reshape(frame,(1,215,215,3)));
+            cos_sin_vector3=model3.predict(np.reshape(frame,(1,215,215,3)));
+            angle=getAngle(cos_sin_vector);
+            angle2=getAngle(cos_sin_vector2)
+            angle3=getAngle(cos_sin_vector3)
+            angle=np.mod(np.mean([angle,angle2,angle3]),120)
+            
+            print(angle);
+            
+            end_time=time.time();
+            print("elapsted time is: "+str(float(end_time-start_time))+" in seconds");
+            
+        
+            
+    cap.release();
+    cv2.destroyAllWindows();
