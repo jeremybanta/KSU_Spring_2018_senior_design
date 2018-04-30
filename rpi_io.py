8b551fe3c98879bc74609e98fb4fb1c73133efc2
Jeremy Banta <37882190+jeremybanta@users.noreply.github.com>
2018-04-30 12:54:09 -0400

    Update rpi_io.py
    add comments

--- rpi_io.py
+++ rpi_io.py
@@ -3,7 +3,7 @@ import time
 
 
 
-def define_inputs_and_out_puts():
+def define_inputs_and_out_puts():  #initalize inputs and outputs on raspberry pi
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(0,GPIO.IN);
@@ -19,7 +19,7 @@ def define_inputs_and_out_puts():
 
 def get_output(number):
 
-    if(number==0):
+    if(number==0):  #if should be moved 15 degrees
 
         GPIO.output(1,GPIO.LOW)
         GPIO.output(2,GPIO.HIGH)
@@ -29,7 +29,7 @@ def get_output(number):
         GPIO.output(6,GPIO.HIGH)
         GPIO.output(7,GPIO.HIGH)
 
-    elif(number==1):
+    elif(number==1): #if should be moved 30 degrees
 
         GPIO.output(1,GPIO.HIGH)
         GPIO.output(2,GPIO.LOW)
@@ -40,7 +40,7 @@ def get_output(number):
         GPIO.output(7,GPIO.HIGH)
 
 
-    elif(number==2):
+    elif(number==2):  #if should be moved 45 degrees
 
         GPIO.output(1,GPIO.HIGH)
         GPIO.output(2,GPIO.HIGH)
@@ -51,7 +51,7 @@ def get_output(number):
         GPIO.output(7,GPIO.HIGH)
 
 
-    elif(number==3):
+    elif(number==3):   #if should be moved 60 degrees
 
         GPIO.output(1,GPIO.HIGH)
         GPIO.output(2,GPIO.HIGH)
@@ -62,7 +62,7 @@ def get_output(number):
         GPIO.output(7,GPIO.HIGH)
 
 
-    elif(number==4):
+    elif(number==4):   #if should be moved 75 degrees
 
         GPIO.output(1,GPIO.HIGH)
         GPIO.output(2,GPIO.HIGH)
@@ -72,7 +72,7 @@ def get_output(number):
         GPIO.output(6,GPIO.HIGH)
         GPIO.output(7,GPIO.HIGH)
 
-    elif(number==5):
+    elif(number==5):   #if should be moved 90 degrees
 
         GPIO.output(1,GPIO.HIGH)
         GPIO.output(2,GPIO.HIGH)
@@ -83,7 +83,7 @@ def get_output(number):
         GPIO.output(7,GPIO.HIGH)
 
 
-    elif(number==6):
+    elif(number==6):  #if should be moved 105 degrees
 
         
         GPIO.output(1,GPIO.HIGH)
@@ -95,7 +95,7 @@ def get_output(number):
         GPIO.output(7,GPIO.HIGH)
         
 
-    elif(number==7):
+    elif(number==7):  #if blue disk should not be moved at all
 
         GPIO.output(1,GPIO.HIGH)
         GPIO.output(2,GPIO.HIGH)
@@ -105,11 +105,17 @@ def get_output(number):
         GPIO.output(6,GPIO.HIGH)
         GPIO.output(7,GPIO.LOW)
 
-    elif(number==8):
+    elif(number==8):  #if blue disk should not be moved at all
 
-        GPIO.output(8,GPIO.LOW);
+        GPIO.output(1,GPIO.HIGH)
+        GPIO.output(2,GPIO.HIGH)
+        GPIO.output(3,GPIO.HIGH)
+        GPIO.output(4,GPIO.HIGH)
+        GPIO.output(5,GPIO.HIGH)
+        GPIO.output(6,GPIO.HIGH)
+        GPIO.output(7,GPIO.LOW)
         
-def turn_off_all_ouput_io_pins();
+def turn_off_all_ouput_io_pins();  #turn all output pins off
 
     GPIO.output(1,GPIO.HIGH)
     GPIO.output(2,GPIO.HIGH)
@@ -120,7 +126,7 @@ def turn_off_all_ouput_io_pins();
     GPIO.output(7,GPIO.HIGH)
         
  
-def digitize_output(angle):
+def digitize_output(angle):  #get int value between 1 and 7 for a angle value between 0 and 120 degrees
     
     if(angle/15==8):
         
